1084f8a978bbf88d22c59bfea48e08c6861a5479
Ivan Ermilov <earthquakesan@gmail.com>
2015-01-16 11:19:16 +0100

    fixed help message for reportgen.jar

--- reportgen/generatereports.sh
+++ reportgen/generatereports.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Generating report for the project ldpo:project_qualisbrasil. The output file is reportqualisbrasil.txt"
-java -jar reportgen.jar "LinkedDataProjectOntology.owl" "ldpo:project_qualisbrasil" > reportqualisbrasil.txt
+echo "Generating report for the project ldpo:project_qualisbrasil. The output file is reportQualisBrasil.txt"
+java -jar reportgen.jar "LinkedDataProjectOntology.owl" "ldpo:project_qualisbrasil" > reportQualisBrasil.txt
 
 echo "Generating report for the project ldpo:project_qualisbrasil. The output file is reportLimes.txt"
 java -jar reportgen.jar "LinkedDataProjectOntology.owl" "ldpo:project_limes_workflowExample" > reportLimes.txt
 
 echo "Generating report for the project ldpo:project_itmolod. The output file is reportItmo.txt"
-java -jar reportgen.jar "LinkedDataProjectOntology.owl" "ldpo:project_limes_workflowExample" > reportItmo.txt
+java -jar reportgen.jar "LinkedDataProjectOntology.owl" "ldpo:project_itmolod" > reportItmo.txt
 
