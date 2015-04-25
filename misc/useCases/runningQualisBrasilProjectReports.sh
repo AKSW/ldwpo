c9c87a5e36f7c9742c3f736752a40665a45054c2
sandrorautenberg <sandro.rautenberg@gmail.com>
2015-04-25 14:07:10 +0200

    minor fix in .sh files

--- misc/useCases/runningQualisBrasilProjectReports.sh
+++ misc/useCases/runningQualisBrasilProjectReports.sh
@@ -8,6 +8,6 @@ echo " the report is generated in reports/report_QualisBrasilConversionProject.h
 
 if [ -f reports/reportQualisBrasil_to_DBpedia_InterlinkingProject.html ]; then rm reports/reportQualisBrasil_to_DBpedia_InterlinkingProject.html; fi
 
-java -jar tools/reportgen.jar "ontology/ldwpo.owl" "ldwpo:project_QualisBrasil_to_DBpedia_Interlinking" > reports/report_QualisBrasilnterlinkProject.html.html
+java -jar tools/reportgen.jar "ontology/ldwpo.owl" "ldwpo:project_QualisBrasil_to_DBpedia_Interlinking" > reports/report_QualisBrasilnterlinkProject.html
 echo " the report is generated in reports/report_QualisBrasilnterlinkProject.html"
 
--- misc/useCases/runningQualisInterlinking.sh
+++ misc/useCases/runningQualisInterlinking.sh
@@ -7,7 +7,7 @@
 
 if [ -f reports/workflowInterlink.html ]; then rm reports/workflowInterlink.html; fi
 
-java -jar tools/ldStackWorkflow.jar "/home/srautenberg/Public/LDWPO/ontology/ldwpo.owl" "ldwpo:project_QualisBrasil_to_DBpedia_Interlinking" "ldwpo:ldWorkflow_execution_interlinking_qualisbrasil_20150126" > reports/workflowInterlink.html
+java -jar tools/ldStackWorkflow.jar "ontology/ldwpo.owl" "ldwpo:project_QualisBrasil_to_DBpedia_Interlinking" "ldwpo:ldWorkflow_execution_interlinking_qualisbrasil_20150126" > reports/workflowInterlink.html
 
 echo "a report is generated in reports/workflowInterlink.html"
 
