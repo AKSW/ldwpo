5e92cd3a28b448696d418d9ecbddff3396ca5cd3
sandrorautenberg <sandro.rautenberg@gmail.com>
2015-11-28 01:20:10 -0200

    qualis csv

--- misc/useCases/Qualis/commands/_extractingFromLegacySystem.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#enviroment stuffs
-mkdir -p Qualis/temp/csv
-if [ -f Qualis/temp/csv/evaluations.csv ]; then rm Qualis/temp/csv/evaluations.csv; fi 
-
-echo "creating evaluations.csv file..."
-mysql -u qualisproject -pqualisproject qualisproject --default-character-set=utf8 -B -e "select * from qualisEvaluationReal;" | sed 's/\t/","/g;s/^/"/;s/$/"/;s/\n//g' > Qualis/temp/csv/evaluations.csv
